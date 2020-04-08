d25c5d2d709d7e10cbce7d250fe3a0b163fb3794
Niccoryan0 <niccoryan0@gmail.com>
2020-04-07 19:08:38 -0700

    css styling changes for the homepage

--- app.js
+++ app.js
@@ -2,17 +2,17 @@
 
 // Create the class Store which will take in location, min customers, max customers, and average cookies per day, will also include an openTime and closeTime just to play around with them, defaults set to 6 and 20 respectively
 function Store(location, minCustomers, maxCustomers, avgCookies, openTime = 6, closeTime = 20) {
-  this.location = location;
+  this.location = location
   this.minCustomers = minCustomers,
   this.maxCustomers = maxCustomers,
   this.avgCookies = avgCookies,
   this.openTime = openTime,
   this.closeTime = closeTime,
-  this.numCustArray = [];
-  this.salesArrayRaw = [],
+  this.numCustArray = [],
+  this.salesArray = [],
   this.salesTotal = 0,
-  this.cookieTossersNeeded = [];
-  this.busyHours = [0.5, 0.75, 1.0, 0.6, 0.8, 1.0, 0.7, 0.4, 0.6, 0.9, 0.7, 0.5, 0.3, 0.4, 0.6];
+  this.cookieTossersNeeded = [],
+  this.busyHours = [0.5, 0.75, 1.0, 0.6, 0.8, 1.0, 0.7, 0.4, 0.6, 0.9, 0.7, 0.5, 0.3, 0.4, 0.6],
   this.hoursOpen = [];
   // Fill the empty hours open with times specific to the given open and close hour, currently makes no differnece since it uses defaults but just playing with it
   for (var i = openTime; i < closeTime; i++) {
@@ -41,10 +41,10 @@ Store.prototype.salesHourlyandTotal = function () {
   for (var i  = this.openTime; i < this.closeTime; i++) {
     var cookiesThisHour = Math.round(this.numCustArray[i-6] * this.avgCookies);
     // cookiesThisHour = Math.round(cookiesThisHour * this.busyHours[i-6]);
-    this.salesArrayRaw.push(cookiesThisHour);
+    this.salesArray.push(cookiesThisHour);
   }
-  for (i = 0; i < this.salesArrayRaw.length; i++) {
-    this.salesTotal = this.salesTotal + this.salesArrayRaw[i];
+  for (i = 0; i < this.salesArray.length; i++) {
+    this.salesTotal = this.salesTotal + this.salesArray[i];
   }
 };
 
@@ -81,12 +81,13 @@ Store.prototype.renderBody = function() {
   for (i = 0; i < this.hoursOpen.length; i++) {
     var currentTRowEl = document.getElementById(this.hoursOpen[i]);
     var newTDataEl = document.createElement('td');
-    newTDataEl.textContent = this.salesArrayRaw[i];
+    newTDataEl.id = 'data';
+    newTDataEl.textContent = this.salesArray[i];
     currentTRowEl.appendChild(newTDataEl);
   }
 };
 
-// Renders the last row of the table, with the first column saying Total and then puts the total day's count for the store in it's final column
+// Renders the total row of the table, with the first column saying Total and then puts the total day's count for the store in it's final column
 Store.prototype.renderFooter = function() {
   // Ensure renderBody is run first so that we can attach footer at the bottom
   this.renderBody();
@@ -94,26 +95,27 @@ Store.prototype.renderFooter = function() {
   var salesTable = document.getElementById('salesTable');
   // Add a total row on the end if it doesn't exist already
   if (!document.getElementById('total')){
-    var lastTRowEl = document.createElement('tr');
-    lastTRowEl.textContent = 'Total';
-    lastTRowEl.id = 'total';
-    salesTable.appendChild(lastTRowEl);
+    var totalTRowEl = document.createElement('tr');
+    totalTRowEl.textContent = 'Total';
+    totalTRowEl.id = 'total';
+    salesTable.appendChild(totalTRowEl);
   }
 
-  // Add total cookie sales for the store to the last box in the store's column
-  var lastTDataEl = document.createElement('td');
-  lastTDataEl.textContent = this.salesTotal;
-  lastTRowEl = document.getElementById('total');
-  lastTRowEl.appendChild(lastTDataEl);
+  // Add total cookie sales for the store to the total box in the store's column
+  var totalTDataEl = document.createElement('td');
+  totalTDataEl.textContent = this.salesTotal;
+  totalTDataEl.id = 'data';
+  totalTRowEl = document.getElementById('total');
+  totalTRowEl.appendChild(totalTDataEl);
 };
 
 Store.prototype.cookieTosserCalc = function () {
   this.renderFooter();
   for(var i = 0; i < (this.closeTime-this.openTime); i ++) {
-    if (this.salesArrayRaw[i] <= 40) {
+    if (this.salesArray[i] <= 40) {
       this.cookieTossersNeeded.push(2);
     } else {
-      this.cookieTossersNeeded.push(Math.ceil(this.salesArrayRaw[i]/20));
+      this.cookieTossersNeeded.push(Math.ceil(this.salesArray[i]/20));
     }
   }
 };
@@ -133,10 +135,10 @@ Store.prototype.cookieTosserTableHeader = function() {
 Store.prototype.cookieTosserTableBody = function() {
   this.cookieTosserTableHeader();
   var tossersTable = document.getElementById('tossersTable');
-  if (!document.getElementById('ct7')){
+  if (!document.getElementById('time7')){
     for (var i = 0; i < this.hoursOpen.length; i++) {
       var newTRowEl = document.createElement('tr');
-      newTRowEl.id = 'ct' + i;
+      newTRowEl.id = 'time' + i;
       newTRowEl.textContent = this.hoursOpen[i];
       tossersTable.appendChild(newTRowEl);
     }
@@ -144,8 +146,9 @@ Store.prototype.cookieTosserTableBody = function() {
 
   // Populate the column for the store with the numbers from the cookieTossersNeeded array
   for (i = 0; i < this.hoursOpen.length; i++) {
-    var currentTRowEl = document.getElementById('ct' + i);
+    var currentTRowEl = document.getElementById('time' + i);
     var newTDataEl = document.createElement('td');
+    newTDataEl.id = 'data';
     newTDataEl.textContent = this.cookieTossersNeeded[i];
     currentTRowEl.appendChild(newTDataEl);
   }
